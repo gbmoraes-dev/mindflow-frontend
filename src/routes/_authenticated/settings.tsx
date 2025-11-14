36165c7a32052f1ce9ccb0c6ecfe3ba32bc7d040
gbmoraes-dev <dev.gbmoraes@gmail.com>
2025-11-14 00:36:42 -0300

    feat: add user informations in settings page

--- src/routes/_authenticated/settings.tsx
+++ src/routes/_authenticated/settings.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { LogOut } from 'lucide-react'
 import { Menu } from '@/components/menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { auth } from '@/lib/auth'
 
@@ -9,6 +10,7 @@ export const Route = createFileRoute('/_authenticated/settings')({
 })
 
 function Settings() {
+  const { user } = Route.useRouteContext()
   const navigate = Route.useNavigate()
 
   async function handleSignOut() {
@@ -20,6 +22,23 @@ function Settings() {
     <div className="h-screen pt-20 pb-5 flex flex-col">
       <div className="flex flex-col items-center justify-center">
         <img src="/mindflow.svg" alt="Mindflow" className="pb-10" />
+      </div>
+
+      <div className="flex flex-row items-center justify-center pb-10">
+        <Avatar>
+          <AvatarImage
+            src={user.image ? user.image : undefined}
+            alt="User avatar"
+          />
+          <AvatarFallback className="bg-teal-500 text-stone-200">
+            {user.name[0]}
+          </AvatarFallback>
+        </Avatar>
+
+        <div className="flex flex-col items-start justify-center pl-3">
+          <p className="text-lg">{user.name}</p>
+          <p className="text-sm text-stone-500">{user.email}</p>
+        </div>
       </div>
 
       <div className="flex flex-col items-center justify-center">
