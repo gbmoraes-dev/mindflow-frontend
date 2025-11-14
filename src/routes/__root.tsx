575ef682cccd7c621be4b8789931afac30fb89b3
gbmoraes-dev <dev.gbmoraes@gmail.com>
2025-11-14 00:36:57 -0300

    feat: add query client

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import * as React from 'react'
 import { Toaster } from '@/components/ui/sonner'
 
-export const Route = createRootRoute({
-  component: RootComponent,
-})
+const queryClient = new QueryClient()
 
-function RootComponent() {
-  return (
-    <React.Fragment>
-      <Toaster position="top-center" />
-      <Outlet />
-    </React.Fragment>
-  )
-}
+const RootLayout = () => (
+  <QueryClientProvider client={queryClient}>
+    <Toaster position="top-center" />
+    <Outlet />
+  </QueryClientProvider>
+)
+
+export const Route = createRootRoute({ component: RootLayout })
