c5a86161b55f14c15d82fcb85d555fb6f618fb0c
gbmoraes-dev <dev.gbmoraes@gmail.com>
2025-11-17 02:31:09 -0300

    feat: add node types

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
-import { env } from '@/env'
 
 export const auth = createAuthClient({
-  baseURL: env.BACKEND_URL,
+  baseURL: process.env.VITE_BACKEND_URL,
 })
